4f23d345114d17df787181e2f2911bd2f1502fc9
ForestEco <jsafronofftc@gmail.com>
2023-11-19 20:19:12 -0800

    First version of simple breakout pong game

---
+++ Breakout_PyGame.py
@@ -0,0 +1,191 @@
+import pygame
+import sys
+import random
+from Brick import Brick
+
+# Initialize Pygame
+pygame.init()
+
+# Set up some constants
+WIDTH, HEIGHT = 800, 600
+BALL_RADIUS = 10
+PADDLE_WIDTH, PADDLE_HEIGHT = 100, 20
+BRICK_WIDTH, BRICK_HEIGHT = 50, 20
+BRICK_COLS, BRICK_ROWS = 10, 5
+
+# Set up the display
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Set up the paddle
+paddle_pos = [WIDTH // 2, HEIGHT - PADDLE_HEIGHT - 10]
+paddle_vel = 12
+
+# Set up the bricks
+bricks = []
+level = 1
+lives = 3
+score = 0
+
+def draw_paddle(paddle_pos, screen):
+    pygame.draw.rect(screen, (255, 255, 255), (paddle_pos[0], paddle_pos[1], PADDLE_WIDTH, PADDLE_HEIGHT))
+class Ball:
+    def __init__(self, pos, vel):
+        self.pos = pos
+        self.vel = vel
+
+    def move(self):
+        self.pos[0] += self.vel[0]
+        self.pos[1] += self.vel[1]
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255, 255, 255), self.pos, BALL_RADIUS)
+
+    def bounce(self, width, height):
+        if self.pos[0] < BALL_RADIUS or self.pos[0] > width - BALL_RADIUS:
+            self.vel[0] *= -1
+        if self.pos[1] < BALL_RADIUS or self.pos[1] > height - BALL_RADIUS:
+            self.vel[1] *= -1
+
+    def update(self):
+        self.pos[0] += self.vel[0]
+        self.pos[1] += self.vel[1]
+
+        # Bounce off the edges of the screen
+        if self.pos[0] < BALL_RADIUS or self.pos[0] > WIDTH - BALL_RADIUS:
+            self.vel[0] *= -1
+        if self.pos[1] < BALL_RADIUS or self.pos[1] > HEIGHT - BALL_RADIUS:
+            self.vel[1] *= -1
+    def check_collision(self, paddle_pos):
+        if self.pos[1] + BALL_RADIUS > paddle_pos[1] and \
+                self.pos[0] + BALL_RADIUS > paddle_pos[0] and \
+                self.pos[0] - BALL_RADIUS < paddle_pos[0] + PADDLE_WIDTH:
+            self.vel[1] *= -1  # Change the ball's y-velocity when it collides with the paddle
+            return True
+        return False
+
+    def set_velocity(self, vel):
+        self.vel = vel
+
+# Initialize the ball
+balls = [Ball([WIDTH // 2, HEIGHT // 2], [0, 0])]  # List of balls
+balls[0].set_velocity([random.randint(1, 5), random.randint(1, 5)])
+
+def draw_bricks(bricks, screen):
+    for brick in bricks:
+        pygame.draw.rect(screen, pygame.Color(brick.color), brick.rect)
+
+def generate_bricks(level):
+    num_bricks = int((WIDTH * HEIGHT * 0.5 * level * 0.05) // (BRICK_WIDTH * BRICK_HEIGHT))
+    for _ in range(num_bricks):
+        col = random.randint(0, BRICK_COLS - 1)
+        row = random.randint(0, BRICK_ROWS - 1)
+        brick = Brick(col * BRICK_WIDTH, row * BRICK_HEIGHT, 'white', BRICK_WIDTH, BRICK_HEIGHT)
+        bricks.append(brick)
+
+generate_bricks(level)
+
+# Game loop
+paused = False
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                paused = not paused
+
+    if not paused:
+        # Move the paddle
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            paddle_pos[0] -= paddle_vel
+        if keys[pygame.K_RIGHT]:
+            paddle_pos[0] += paddle_vel
+
+        # Make sure the paddle doesn't go off the screen
+        if paddle_pos[0] < 0:
+            paddle_pos[0] = 0
+        if paddle_pos[0] > WIDTH - PADDLE_WIDTH:
+            paddle_pos[0] = WIDTH - PADDLE_WIDTH
+
+        for ball in balls:
+            ball.update()
+            ball.draw(screen)
+            # Move the ball
+            ball.move()
+
+            # Bounce the ball off the walls
+            ball.bounce(WIDTH, HEIGHT)
+
+            # Bounce the ball off the paddle
+            ball.check_collision(paddle_pos)
+
+            # Check for collision with bottom of screen
+            if ball.pos[1] > HEIGHT - 10:
+                lives -= 1
+                if lives == 0:
+                    print("You lose")
+                    pygame.quit()
+                    sys.exit()
+                else:
+                    # Reset the ball's position and velocity
+                    ball.pos = [WIDTH // 2, HEIGHT // 2]
+
+            # Check for ball hitting a brick
+            for brick in bricks:
+                if brick.rect.collidepoint(ball.pos):
+                    if brick.color == 'white':
+                        lives += 1
+                    elif brick.color == 'red':
+                        PADDLE_WIDTH *= 0.5
+                    elif brick.color == 'green':
+                        lives += 1
+                    elif brick.color == 'blue':
+                        PADDLE_WIDTH *= 1.25
+                    elif brick.color == 'yellow':
+                        PADDLE_HEIGHT *= 1.5
+                    elif brick.color == 'magenta':
+                        PADDLE_HEIGHT *= 0.75
+                    elif brick.color == 'cyan':
+                        lives += 2
+                         #increase ball velocity?
+                    elif brick.color == 'silver':
+                        paddle_vel += 2
+                        # Create a new ball instance BUG - fix the drawing of all the different ping pong balls updating screen method
+                        #new_ball = Ball([WIDTH // 2, HEIGHT // 2], [random.randint(2, 5), random.randint(2, 5)])
+                        #balls.append(new_ball)
+                    bricks.remove(brick)
+                    ball.vel[1] *= -1
+                    score += 1
+
+            # Check if all bricks are destroyed
+            if len(bricks) == 0:
+                if score >= 100:
+                    lives += 1
+                level += 1
+                generate_bricks(level)
+                print("You win! Starting level ", level)
+
+    # Draw everything
+    screen.fill((0, 0, 0))
+    # Draw the score and lives
+    draw_paddle(paddle_pos, screen)
+    font = pygame.font.Font(None, 36)
+    text = font.render("Score: " + str(score) + " Lives: " + str(lives), 1, (255, 255, 255))
+    screen.blit(text, (10, 10))
+
+    # Draw bricks
+    draw_bricks(bricks, screen)
+
+    # Draw the ball
+    ball.draw(screen)
+
+    # Draw the paddle
+    pygame.draw.rect(screen, (255, 255, 255), (paddle_pos[0], paddle_pos[1], PADDLE_WIDTH, PADDLE_HEIGHT))
+
+    # Update the display
+    pygame.display.flip()
+
+    # Limit the frame rate
+    pygame.time.Clock().tick(60)
---
+++ Brick.py
@@ -0,0 +1,32 @@
+import pygame
+import random
+class Brick:
+    def __init__(self, x, y, color, width, height):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.width = width
+        self.height = height
+        self.rect = pygame.Rect(x, y, width, height)
+        if random.random() < 0.1:
+            self.color = 'red'
+        else:
+            self.color = random.choice(['green', 'blue', 'cyan', 'magenta', 'yellow', 'silver'])
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
+
+    def check_collision(ball, bricks, paddle):
+        for brick in bricks:
+            if ball.rect.colliderect(brick.rect):
+                if brick.color == 'red':
+                    paddle.vel += 2
+                    ball.pos = [WIDTH // 2, HEIGHT // 2]
+                elif brick.color == 'silver':
+                    paddle.vel += 2
+                    ball.pos = [WIDTH // 2, HEIGHT // 2]
+                    # Add another ball
+                    balls.append(Ball(WIDTH // 2, HEIGHT // 2))
+                bricks.remove(brick)
+                return True
+        return False
